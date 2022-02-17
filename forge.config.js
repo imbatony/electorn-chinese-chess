8a6ae18eaefc5ba6661dd4aefe1267046d528ab1
esfak47 <esfak47@qq.com>
2022-02-18 02:06:02 +0800

    v0.0.1

---
+++ forge.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "packagerConfig": {
+        "name": "中国象棋",
+        "icon": "./assets/img/chess.ico"
+    },
+    "makers": [
+        {
+            "name": "@electron-forge/maker-squirrel",
+            "config": {
+                "name": "electorn_chinese_chess"
+            }
+        },
+        {
+            "name": "@electron-forge/maker-zip",
+            "platforms": [
+                "darwin"
+            ]
+        },
+        {
+            "name": "@electron-forge/maker-deb",
+            "config": {}
+        },
+        {
+            "name": "@electron-forge/maker-rpm",
+            "config": {}
+        }
+    ],
+    "plugins": [
+        [
+            "@electron-forge/plugin-webpack",
+            {
+                "mainConfig": "./webpack.main.config.js",
+                "renderer": {
+                    "config": "./webpack.renderer.config.js",
+                    "entryPoints": [
+                        {
+                            "html": "./src/renderer/index.html",
+                            "js": "./src/renderer/renderer.ts",
+                            "name": "main_window"
+                        }
+                    ]
+                }
+            }
+        ]
+    ]
+}
---
+++ src/renderer/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(<h2>中国象棋</h2>, document.getElementById("root"));
+}
+
+render(); 
